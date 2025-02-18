88950a28a9cc4d629cbf87f8dbc384ca20fc5fee
root <root@ip-172-31-24-62.us-west-2.compute.internal>
2025-02-18 04:07:14 +0000

    two numbers addition done

--- sample.java
+++ sample.java
@@ -1,4 +1,12 @@
 public static void main(String [] args)
 {
-	System.out.println("my git");
+     int num1;
+     int num2;
+
+     int sum;
+
+     sum = num1+num2;
+
+     System.out.println("addition of two numbers is: " + sum);
 }
+
